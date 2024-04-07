ada567a2e83a64b96158cb9fa97af01c315269d1
noqcks <benji@093b.org>
2024-04-07 03:59:10 +0000

    Upgrade dependencies

--- src/authentication/postSecureNumbers.ts
+++ src/authentication/postSecureNumbers.ts
@@ -9,14 +9,15 @@ export const postSecureNumbers = async (
   let [secureNo1, secureNo2, secureNo3] = secureNumbers;
   await request.post('https://online.hl.co.uk/my-accounts/login-step-two', {
     body: JSON.stringify({      hl_vt: hl_vt,      'online-password-verification': password,      'secure-number[1]': secureNumber[secureNo1],      'secure-number[2]': secureNumber[secureNo2],      'secure-number[3]': secureNumber[secureNo3],      submit: 'Log+in',    }),
-    json: {
+    json: true,
+    body: JSON.stringify({
       hl_vt: hl_vt,
       'online-password-verification': password,
       'secure-number[1]': secureNumber[secureNo1],
       'secure-number[2]': secureNumber[secureNo2],
       'secure-number[3]': secureNumber[secureNo3],
       submit: 'Log+in',
-    },
+    }),
     followRedirect: false,
   });
 };
